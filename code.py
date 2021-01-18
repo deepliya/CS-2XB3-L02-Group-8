f5b58d16cbe59a35bd2c6f57b3e84af3025e9f3e
CozyChristopher <59571563+CozyChristopher@users.noreply.github.com>
2021-01-18 17:20:28 -0500

    removed code.py contents

--- code.py
+++ code.py
@@ -1,9 +1,1 @@
-def are_valid_groups(stdno, groups):
-    counter = 0
-    for i in groups:
-        for j in range (len(stdno)):
-            if stdno[j] in i:
-                counter += 1
-    if counter != len(stdno)
-        return False
-    return True
+
