c5dfcbaaec4b02170e999013e32463036bd06365
deepliya <50700950+deepliya@users.noreply.github.com>
2021-01-18 16:51:32 -0500

    defined functin 'are_valid_groups'

--- code.py
+++ code.py
@@ -1,9 +1,9 @@
-def are_valid_groups(student_nums, groups_lst):
+def are_valid_groups(stdno, groups):
     counter = 0
-    for i in groups_lst:
-        for j in range(len(student_nums)):
-            if student_nums[j] in i:
-                counter += 1
-    if counter == len(student_nums):
-        return True
-    return False
+    for i in groups:
+        for j in range (len(stdno)):
+            if stdno[j] in i:
+                counter = counter + 1
+    if counter != len(stdno)
+        return False
+    return True
