4f616749a6679d3ed704427378ba69c01d7f5331
CozyChristopher <59571563+CozyChristopher@users.noreply.github.com>
2021-01-18 17:10:10 -0500

    Pushing from main branch with 1 change

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ def are_valid_groups(stdno, groups):
     for i in groups:
         for j in range (len(stdno)):
             if stdno[j] in i:
-                counter = counter + 1
+                counter += 1
     if counter != len(stdno)
         return False
     return True
