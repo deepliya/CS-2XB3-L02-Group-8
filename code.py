0870183a82da9f66f4cf79c65c73813025e18eeb
Victor <victorkabugo@gmail.com>
2021-01-18 17:04:00 -0500

    Added are_valid_groups function

--- code.py
+++ code.py
@@ -1,1 +1,17 @@
-This is a python file.
+def are_valid_groups(student, groups):
+
+    for x in student:
+
+        found = False
+
+        for y in groups:
+
+            if x in y:
+
+                found = True
+
+        if not found:
+
+            return found
+
+    return True
